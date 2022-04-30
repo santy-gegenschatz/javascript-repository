26ffc3488be50960804b5d83251ee39bf06e0c07
santy-gegenschatz <gegenschatz.santiago@gmail.com>
2022-04-30 11:55:42 -0300

    Inclui un ejemplo con un loop tipo while

--- Entregable1/index.js
+++ Entregable1/index.js
@@ -11,15 +11,27 @@ if (operacion == "FACTORIAL") { // Realizamos el factorial
     let factorial = 1;
     for (let i = 1; i <= numero_ingresado; i++) {
         factorial = i * factorial;
+        console.log("Calculando: ", factorial);
     }
     respuesta = factorial;
 } else if (operacion == "SUMA ITERADA") { // Realizamos la suma iterada
     let suma_iterada = 0;
     for (let i = 0; i <= numero_ingresado; i++) {
         suma_iterada = suma_iterada + i;
+        console.log("Calculando: ", suma_iterada);
     }
     respuesta = suma_iterada
 } else {
     respuesta = mensaje_de_error;
 }
 console.log(respuesta);
+
+let condicion = false;
+while (condicion == false) {
+       console.log("El programa está corriendo");
+       let palabra_ingresada = prompt("Ingrese la palabra frenar para detener el programa");
+       palabra_ingresada = palabra_ingresada.toUpperCase();
+       if (palabra_ingresada === "FRENAR") {
+           condicion = true;
+       }
+}
