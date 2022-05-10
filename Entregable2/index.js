7e45f6f609f5d2dc939649e6b719700e363749fb
santy-gegenschatz <gegenschatz.santiago@gmail.com>
2022-05-10 20:54:24 -0300

    Entregable 2

---
+++ Entregable2/index.js
@@ -0,0 +1,11 @@
+// Vamos a simular 
+alert("Hola! Bienvenido al simulador de ");
+function factorial(param1) {
+    for (let i = 0; i < 10; i++) {
+        
+    }
+}
+
+function utilizaUnCondicional(param1) {
+
+}
