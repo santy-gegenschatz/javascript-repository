48d1e1c765da0b2498e7d364e3e84a818151d115
santy-gegenschatz <gegenschatz.santiago@gmail.com>
2022-05-10 21:03:14 -0300

    Entregable 2 mejorado

--- Entregable2/index.js
+++ Entregable2/index.js
@@ -1,11 +1,19 @@
 // Vamos a simular 
-alert("Hola! Bienvenido al simulador de ");
+alert("Hola! Bienvenido al simulador de procesos matemáticos");
+let factorial = Number(prompt("Ingrese un número al cual calcularle un factorial"));
+let esPar = Number(prompt("Ingrese un número para saber si es par o no"));
 function factorial(param1) {
+    factorial = 1;
     for (let i = 0; i < 10; i++) {
-        
+        factorial*=i;
     }
+    alert("El factorial de " + param1 +" es " + factorial);
 }
 
-function utilizaUnCondicional(param1) {
-
+function esPar(param1) {
+    if (param1%2 == 0) {
+        alert("Es par");
+    } else {
+        alert("Es impar");
+    }
 }
