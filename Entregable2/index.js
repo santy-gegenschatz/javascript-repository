89dc809285a93086bde1ae52000b44d50d7e80ec
santy-gegenschatz <gegenschatz.santiago@gmail.com>
2022-05-19 13:54:26 -0300

    Entregable 3

--- Entregable2/index.js
+++ Entregable2/index.js
@@ -1,20 +1,22 @@
 // Vamos a simular 
 alert("Hola! Bienvenido al simulador de procesos matemáticos");
-let factorial_num = Number(prompt("Ingrese un número al cual calcularle un factorial"));
 factorial(factorial_num);
-let esPar_num = Number(prompt("Ingrese un número para saber si es par o no"));
 esPar(esPar_num);
 
-function factorial(param1) {
+// Definición de una función con un loop for
+function factorial() {
+    let factorial_num = Number(prompt("Ingrese un número al cual calcularle un factorial"));
     let factorial = 1;
-    for (let i = 1; i <= param1; i++) {
+    for (let i = 1; i <= factorial_num; i++) {
         factorial*=i;
     }
-    alert("El factorial de " + param1 + " es " + factorial);
+    alert("El factorial de " + factorial_num + " es " + factorial);
 }
 
-function esPar(param1) {
-    if (param1%2 == 0) {
+// Definición de una función con un condicional
+function esPar() {
+    let esPar_num = Number(prompt("Ingrese un número para saber si es par o no"));
+    if (esPar_num%2 == 0) {
         alert("Es par");
     } else {
         alert("Es impar");
