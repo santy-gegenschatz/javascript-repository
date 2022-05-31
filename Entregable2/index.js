f207476f7675a482913edc8929b352dc966cf0bd
santy-gegenschatz <gegenschatz.santiago@gmail.com>
2022-05-31 18:02:32 -0300

    Se añade en el entregable 6

--- Entregable2/index.js
+++ Entregable2/index.js
@@ -1,24 +1,37 @@
-// Vamos a simular 
-alert("Hola! Bienvenido al simulador de procesos matemáticos");
-factorial(factorial_num);
-esPar(esPar_num);
-
-// Definición de una función con un loop for
-function factorial() {
-    let factorial_num = Number(prompt("Ingrese un número al cual calcularle un factorial"));
+// El primer ejercicio es un programa que corra un bucle.
+// Vamos a utilizar un bucle for
+let numero_ingresado = Number(prompt("Por favor ingrese un número"));
+let operacion = prompt(`¿Qué operación quiere realizar?:
+                        FACTORIAL
+                        SUMA ITERADA`);
+operacion = operacion.toUpperCase();
+let respuesta;
+let mensaje_de_error = "Oops, esa no es una opción válida";
+if (operacion == "FACTORIAL") { // Realizamos el factorial
     let factorial = 1;
-    for (let i = 1; i <= factorial_num; i++) {
-        factorial*=i;
+    for (let i = 1; i <= numero_ingresado; i++) {
+        factorial = i * factorial;
+        console.log("Calculando: ", factorial);
     }
-    alert("El factorial de " + factorial_num + " es " + factorial);
+    respuesta = factorial;
+} else if (operacion == "SUMA ITERADA") { // Realizamos la suma iterada
+    let suma_iterada = 0;
+    for (let i = 0; i <= numero_ingresado; i++) {
+        suma_iterada = suma_iterada + i;
+        console.log("Calculando: ", suma_iterada);
+    }
+    respuesta = suma_iterada
+} else {
+    respuesta = mensaje_de_error;
 }
+console.log(respuesta);
 
-// Definición de una función con un condicional
-function esPar() {
-    let esPar_num = Number(prompt("Ingrese un número para saber si es par o no"));
-    if (esPar_num%2 == 0) {
-        alert("Es par");
-    } else {
-        alert("Es impar");
-    }
+let condicion = false;
+while (condicion == false) {
+       console.log("El programa está corriendo");
+       let palabra_ingresada = prompt("Ingrese la palabra frenar para detener el programa");
+       palabra_ingresada = palabra_ingresada.toUpperCase();
+       if (palabra_ingresada === "FRENAR") {
+           condicion = true;
+       }
 }
--- Entregable3/index.js
+++ Entregable3/index.js
@@ -1,195 +1,24 @@
-// Constantes globales
-let usuarios = []
-let pagos = []
-let vendedores = []
-let productos = []
-let id_usuarios_count = 0;
-let id_pagos_count = 0;
+// Vamos a simular 
+alert("Hola! Bienvenido al simulador de procesos matemáticos");
+factorial(factorial_num);
+esPar(esPar_num);
 
-// Clases del sistema a simular
-class Usuario {
-    constructor(unique_id, nombre, apellido, dni, saldo) {
-        this.unique_id = unique_id;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.dni = dni;
-        this.saldo = saldo;
+// Definición de una función con un loop for
+function factorial() {
+    let factorial_num = Number(prompt("Ingrese un número al cual calcularle un factorial"));
+    let factorial = 1;
+    for (let i = 1; i <= factorial_num; i++) {
+        factorial*=i;
     }
-
-    transferir(usuario, monto) {
-        if (validarSaldo()) {
-            this.saldo -= monto;
-            usuario.saldo += monto;
-            const pago = new Pago(id_pagos_count + 1, monto);
-            pagos.push(pago);
-        } else {
-            alert("Eres pobre y no puedas realizar esta acción");
-        }
-    }
-
-    validarSaldo(monto) {
-        return this.saldo >= monto;
-    }
+    alert("El factorial de " + factorial_num + " es " + factorial);
 }
 
-class Pago {
-    constructor(unique_id, monto) {
-        this.unique_id = unique_id;
-        this.monto = monto;
-    }
-}
-
-class Vendedor {
-    constructor(unique_id, nombre, apellido, saldo) {
-        this.unique_id = unique_id;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.saldo = saldo;
-    }
-}
-
-class Producto {
-    constructor(unique_id, nombre, precio, stock) {
-        this.unique_id = unique_id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.stock = stock;
-    }
-}
-
-// Métodos
-// Administrar que hacer
-function administrar() {
-    let orden = prompt(`Seleccione una opción:
-                        1. Crear Usuario
-                        2. Ver Usuarios existentes
-                        3. Cargar Saldo
-                        4. Transferir entre Usuarios
-                        5. Eliminar Usuario`);
-    switch (orden) {
-        case ("1") :
-            console.log("Eligió el caso 1");
-            altaUsuario();
-            break;
-        case ("2") :
-            console.log("Eligió el caso 2");
-            imprimirUsuarios();
-            break;
-        case ("3") :
-            console.log("Eligió el caso 3");
-            cargarSaldo();
-            break;
-        case ("4") :
-            console.log("Eligió el caso 4");
-            transferencia();
-            break;
-        case ("5") :
-            console.log("Eligió el caso 5");
-            eliminarUsuario();
-            break;
-        default:
-            console.log("Ninguna de las opciones seleccionas es válida, gil.")
-            
-    }
-}
-
-// Dar de alta
-function altaUsuario() {
-    let nombre = prompt("Ingrese el nombre");
-    let apellido = prompt("Ingrese el apellido");
-    let dni = prompt("Ingrese el dni");
-
-    let usuario = new Usuario(id_usuarios_count + 1, nombre, apellido, dni, 0);
-    usuarios.push(usuario);
-    console.log(usuario);
-    administrar();
-}
-
-// Dar de baja
-function eliminarUsuario() {
-    let dni = prompt("Ingrese el dni del usuario a eliminar");
-    if (usuarioExiste(dni)) {
-        let usuario = usuarios.find((user) => user.dni === dni);
-        usuarios.pop(usuario);
-        alert("Eliminado con éxito.")
-        administrar();
+// Definición de una función con un condicional
+function esPar() {
+    let esPar_num = Number(prompt("Ingrese un número para saber si es par o no"));
+    if (esPar_num%2 == 0) {
+        alert("Es par");
     } else {
-        alert("El dni ingresado no concuerda con el de ningún usuario en el sistema.")
-        eliminarUsuario();
-    }
-
-}
-
-// Imprimir usuarios existentes
-function imprimirUsuarios() {
-    if (usuarios.length > 0) {
-        let alert_string = "Usuario" + "\n";
-        for (let i = 0; i < usuarios.length; i++) {
-            let user_i = usuarios[i];
-            alert_string +=  user_i.nombre;
-            alert_string +=  "\n" + user_i.apellido;
-            alert_string +=  "\n" + user_i.dni;
-            alert_string += "\n" + "----" + "\n";
-        }
-        alert(alert_string); 
-    }  else {
-        alert("No hay usuarios existentes aún.");
-    }
-    administrar();
-}
-
-// Cargar Saldo
-function cargarSaldo() {
-    let dni = prompt("Ingrese el dni del usuario a enriquecer")
-    if (usuarioExiste(dni)) {
-        const usuario = usuarios.find((user) => user.dni = dni);
-        // Obtener la info del monto a acreditar
-        let monto = prompt("Ingrese un monto:")
-        usuario.monto+=monto; 
-    } else {
-        alert("El dni ingresado no coincide con un usuario en la base de datos");
-    }
-    // Volver al menú
-    administrar();
-}
-
-// Buscar un usuario
-function usuarioExiste(dni) {
-    if (typeof(usuarios.find((user) => user.dni === dni)) === 'undefined') {
-        return false
-    } else {
-        return true
+        alert("Es impar");
     }
 }
-
-// Hacer un pago
-function transferencia() {
-    let dni_origen = prompt("Ingrese el dni de quien transfiere");
-    if (usuarioExiste(dni_origen)) {
-        let dni_destino = prompt("Ingrese el dni de quien recibe");
-        if (usuarioExiste(dni_destino)) {
-            usuario_origen = usuarios.find((u) => u.dni == dni_origen);
-            usuario_destino = usuarios.find((u) => u.dni == dni_destino);
-            let monto = prompt("Ingrese un monto a transferir");
-            usuario_origen.transferencia(usuario_destino, monto);
-        } else {
-            alert("Ingrese un Dni válido")
-        }
-    } else {
-        alert("Ingrese un Dni válido")
-    }
-    administrar();
-}
-// 
-
-
-// Agregar dos usuarios para poder hacer testings
-const usuario1 = new Usuario (1, "Santy", "Gegenschatz", "1", "100");
-const usuario2 = new Usuario (2, "John", "Doe", "2", "100");
-usuarios.push(usuario1);
-usuarios.push(usuario2);
-
-// Arrancar la aplicación
-alert(usuarioExiste(5));
-administrar();
-
